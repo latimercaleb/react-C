7bad2676cc5a035d66278e6a6193935b9e6ef0d4
latimercaleb <nohitokaze@gmail.com>
2020-11-26 13:47:42 -0500

    Starting lifecycle hook practice

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -5,28 +5,51 @@ class App extends React.Component{
     super(props)
     this.state = {
       Latitude: null,
-      Longitude: null
+      Longitude: null,
+      ErrorMessage: ''
     };
+  }
 
+  componentDidMount(){
+    console.log('Mount called: Component displayed');
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
-        console.log(position);
         this.setState({
           Latitude: position.coords.latitude,
           Longitude: position.coords.longitude
         })
       },
-      (err) => console.log(err)
+      (err) => this.setState({ErrorMessage: err.code +'- ' + err.message})
     );
   }
 
+  componentDidUpdate(){
+    console.log('Update called: Component is re-rendered');
+  }
+
   render(){
-    return( 
-      <div>
-        <p>Latitude: {this.state.Latitude} </p>
-        <p>Longitude: {this.state.Longitude} </p>
-      </div>
-    );
+    if(this.state.ErrorMessage){
+      return( 
+        <div>
+          <p>Error: {this.state.ErrorMessage}</p>
+        </div>
+      );
+    }
+    else if(this.state.Latitude){
+      return(
+        <div>
+          <p>Latitude: {this.state.Latitude} </p>
+          <p>Longitude: {this.state.Longitude} </p>
+        </div>
+      );
+    }
+   else{
+     return(
+       <div>
+         Loading...
+       </div>
+     );
+   }
   }
 }
 
