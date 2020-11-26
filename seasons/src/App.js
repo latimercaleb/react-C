2033efc4642735e38ed27de87f7e6d89ac777977
latimercaleb <nohitokaze@gmail.com>
2020-11-26 01:32:27 -0500

    Adding class components and state practice

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -1,26 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  // Should port over translator app as well
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      Latitude: null,
+      Longitude: null
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+        this.setState({
+          Latitude: position.coords.latitude,
+          Longitude: position.coords.longitude
+        })
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  render(){
+    return( 
+      <div>
+        <p>Latitude: {this.state.Latitude} </p>
+        <p>Longitude: {this.state.Longitude} </p>
+      </div>
+    );
+  }
 }
 
 export default App;
