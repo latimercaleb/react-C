669ddd3e2fea6b5ed9fae0f03d94b8b42a774721
Callat <latimercaleb@gmail.com>
2021-08-26 00:23:04 -0400

    Picking back up side proj

--- yt-clone/src/components/VideoItem.js
+++ yt-clone/src/components/VideoItem.js
@@ -1,1 +1,7 @@
 // Contains sub-video captions
+import React from 'react';
+const VideoItem = (props) => {
+    return <div>Video Item</div>
+};
+
+export default VideoItem;
--- yt-clone/src/components/VideoList.js
+++ yt-clone/src/components/VideoList.js
@@ -1,8 +1,12 @@
 // List of videos
 import React from 'react';
+import VideoItem from './VideoItem';
 
-const VideoList = props => {
-
+const VideoList = ({videos}) => {
+    const renderedList = videos.map((video) => {
+        return <VideoItem />;
+    });
+    return <div>{renderedList}</div>
 };
 
 export default VideoList;
