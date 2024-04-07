2549098a539ad7ff3f8c980de63b0e2e41b64cbb
Exia <nohitokaze@gmail.com>
2024-04-07 14:32:46 -0400

    Outlining new project

--- new-jsx-practice/src/App.js
+++ new-jsx-practice/src/App.js
@@ -1,27 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import ProfileCard from "./ProfileCard";
 
-function App() {
-  const text = "Hello world!"
-  const textColor = "teal"
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <b color='teal' style={{fontSize: "60px", color: "teal"}}>{text}</b>
-        <input autoFocus></input>
-        <input placeholder="hi there" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div>
+            <h1>Sample App: Personal Assistant Store</h1>
+        </div>
+    );
 }
-
-export default App;
