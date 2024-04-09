ebae3c2af9fe6f1561da35f5b2a1fb59e3d07d6f
Exia <nohitokaze@gmail.com>
2024-04-08 22:56:28 -0400

    Adding some prop work, review later

--- new-jsx-practice/src/App.js
+++ new-jsx-practice/src/App.js
@@ -1,9 +1,17 @@
 import ProfileCard from "./ProfileCard";
-
+import AlexImg from './images/alexa.png'
+import CortanaImg from './images/cortana.png'
+import SiriImg from './images/siri.png'
 export default function App() {
     return (
         <div>
             <h1>Sample App: Personal Assistant Store</h1>
+            <ProfileCard title="Chat GPT" handle="@chatgpt" coreImg=""></ProfileCard>
+            <ProfileCard title="Siri" handle="@hey_siri" coreImg={SiriImg}></ProfileCard>
+            <ProfileCard title="Cortana" handle="@cortana" coreImg={CortanaImg}></ProfileCard>
+            <ProfileCard title="Ok, Google" handle="@google" coreImg=""></ProfileCard>
+            <ProfileCard title="Bixby" handle="@bixby" coreImg=""></ProfileCard>
+            <ProfileCard title="Alexa" handle="@alexa" coreImg={AlexImg}></ProfileCard>
         </div>
     );
 }
--- new-jsx-practice/src/ProfileCard.js
+++ new-jsx-practice/src/ProfileCard.js
@@ -1,7 +1,11 @@
-function ProfileCard(){
-    return(<div>
-        Profile Card
-    </div>)
+function ProfileCard({title, handle, coreImg}){
+    return(
+        <div>
+            <h3>{title}</h3>
+            <img src={coreImg}></img>
+            <small><i>{handle}</i></small>
+        </div>
+    )
 }
 
 export default ProfileCard;
