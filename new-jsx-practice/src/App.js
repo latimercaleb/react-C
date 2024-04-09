c5cf4c52fc5dbf27800542131661227c89fa5111
Exia <nohitokaze@gmail.com>
2024-04-09 13:01:59 -0400

    Adding further styles and some other details

--- new-jsx-practice/src/App.js
+++ new-jsx-practice/src/App.js
@@ -1,51 +1,71 @@
-import ProfileCard from "./ProfileCard";
-import AlexImg from './images/alexa.png'
-import CortanaImg from './images/cortana.png'
-import SiriImg from './images/siri.png'
+import ProfileCard from './ProfileCard';
+import AlexImg from './images/alexa.png';
+import CortanaImg from './images/cortana.png';
+import SiriImg from './images/siri.png';
+import BixbyImg from './images/bixby.png';
+import ChatGPT from './images/chat-gpt-logo.png';
+import Google from './images/google-assistant.png';
 import 'bulma';
 
 export default function App() {
-    // TODO: Add images for other links
-    // Do additional styling in bulma
-    // Get prettier to work so you dont have to manually format while doing this, wasting alot of time
-    // Add description prop
-    // Do something about custom component self-closing tag
-    return (
-        <div>
-            <section className="hero is-primary">
-                <div className="hero-body">
-                    <p className="title">Sample App 1: Personal Assistant Store</p>
-                </div>
-            </section>
-            <div className="container">
-                <section className="section">
-                    <div className="columns">
-                        <div className="column is-4">
-                        <ProfileCard title="Chat GPT" handle="@chatgpt" coreImg="" />
-                        </div>
-                        <div className="column is-4">
-                        <ProfileCard title="Siri" handle="@hey_siri" coreImg={SiriImg}></ProfileCard>
-                        </div>
-                        <div className="column is-4">
-                        <ProfileCard title="Cortana" handle="@cortana" coreImg={CortanaImg}></ProfileCard>
-                        </div>
-                    </div>
-                    <div className="columns">
-                    <div className="column is-4">
-
-                    </div>
-                    <div className="column is-4">
-
-                    </div>
-                    <div className="column is-4">
-
-                    </div>
-                    </div>
-                </section>
-            </div>
-            <ProfileCard title="Ok, Google" handle="@google" coreImg=""></ProfileCard>
-            <ProfileCard title="Bixby" handle="@bixby" coreImg=""></ProfileCard>
-            <ProfileCard title="Alexa" handle="@alexa" coreImg={AlexImg}></ProfileCard>
-        </div>
-    );
+	return (
+		<div>
+			<section className="hero is-info">
+				<div className="hero-body">
+					<p className="title">Sample App 1: Personal Assistant Store</p>
+				</div>
+			</section>
+			<div className="container">
+				<section className="section">
+					<div className="columns">
+						<div className="column is-4">
+							<ProfileCard
+								title="Chat GPT"
+								handle="@chatgpt"
+								coreImg={ChatGPT}
+								desc="Chat GPT How can I help you today?"
+							/>
+						</div>
+						<div className="column is-4">
+							<ProfileCard
+								title="Siri"
+								handle="@hey_siri"
+								coreImg={SiriImg}
+								desc="Get your every day tasks done using your voice"></ProfileCard>
+						</div>
+						<div className="column is-4">
+							<ProfileCard
+								title="Cortana"
+								handle="@cortana"
+								coreImg={CortanaImg}
+								desc="Cortana the microsoft assistant!"></ProfileCard>
+						</div>
+					</div>
+					<div className="columns">
+						<div className="column is-4">
+							<ProfileCard
+								title="Ok, Google"
+								handle="@google"
+								coreImg={Google}
+								desc="Google Assistant your hands-free helper!"></ProfileCard>
+						</div>
+						<div className="column is-4">
+							<ProfileCard
+								title="Bixby"
+								handle="@bixby"
+								coreImg={BixbyImg}
+								desc="Do more with Samsung Bixby!"></ProfileCard>
+						</div>
+						<div className="column is-4">
+							<ProfileCard
+								title="Alexa"
+								handle="@alexa"
+								coreImg={AlexImg}
+								desc="Alexa the amazon assistant"></ProfileCard>
+						</div>
+					</div>
+				</section>
+			</div>
+		</div>
+	);
 }
--- new-jsx-practice/src/ProfileCard.js
+++ new-jsx-practice/src/ProfileCard.js
@@ -1,19 +1,24 @@
-function ProfileCard({title, handle, coreImg}){
-    return(
-        <div className="card">
-            <div className="card-image">
-                <figure className="image-is-1by1">
-                    <img src={coreImg} alt={coreImg}></img>
-                </figure>
-            </div>
-            <div className="card-content">
-                <div className="media-content">
-                    <p className="title is-4">{title}</p>
-                    <p className="subtitle is-6"><i>{handle}</i></p>
-                </div>
-            </div>
-        </div>
-    )
+function ProfileCard({ title, handle, coreImg, desc }) {
+	return (
+		<div className="card">
+			<div className="card-image">
+				<figure className="image-is-1by1">
+					<img
+						src={coreImg}
+						alt={coreImg}></img>
+				</figure>
+			</div>
+			<div className="card-content">
+				<div className="media-content">
+					<p className="title is-4">{title}</p>
+					<p className="subtitle is-6">
+						<i>{handle}</i>
+					</p>
+                    <p className="is-capitalized has-text-weight-semibold ">{desc}</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ProfileCard;
