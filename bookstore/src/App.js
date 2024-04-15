07b0e5065b5077c925c33c21313ff1c01f0a1515
Exia <nohitokaze@gmail.com>
2024-04-15 01:24:10 -0400

    Wiring up event handler and propogation, holding here

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -1,17 +1,22 @@
-import useState from 'react'
-import './index.css'
+import { useState } from 'react';
+import BookNew from './components/BookNew'
+import BookShelf from './components/BookShelf';
 function App() {
   const [bookData, setBookData] = useState([]);
   const handleAddNewBook = (newBook) => {
-    setBookData([...bookData, newBook])
+    console.log('In parent handler');
+    console.log(newBook);
+    const n = [...bookData, newBook];
+    setBookData(n)
+    console.log(bookData)
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>App 4: The bookstore</h1>
-        <BookNew></BookNew>
-        <BookShelf></BookShelf>
+        <BookNew newBook={handleAddNewBook}></BookNew>
+        <BookShelf ></BookShelf>
       </header>
     </div>
   );
--- bookstore/src/components/BookNew.js
+++ bookstore/src/components/BookNew.js
@@ -1,9 +1,34 @@
-function BookNew() {
-    return (
-        <div>
-            Render new book create logic
-        </div>
-    );
+import { useState } from 'react';
+function BookNew({ newBook }) {
+	const [bookTitle, setBookTitle] = useState('');
+	const handleNewBookTitle = (inputBookTitleEvent) => {
+		console.log(inputBookTitleEvent);
+		const newBookTitle = inputBookTitleEvent.target.value;
+		setBookTitle(newBookTitle);
+	};
+	const handleSubmit = (submitEvent) => {
+		console.log(submitEvent);
+		submitEvent.preventDefault();
+		newBook(bookTitle);
+		setBookTitle('');
+	};
+	return (
+		<div className='book-create'>
+            <h3>Add a new book</h3>
+			<form onSubmit={handleSubmit}>
+				<div >
+					<label htmlFor="Book Create" >Title</label>
+					<input
+						type="text"
+						value={bookTitle}
+						onChange={handleNewBookTitle}
+                        className='input'
+					/>
+					<button className='button'>Create New Book</button>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default BookNew;
