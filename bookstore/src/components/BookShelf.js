ee3a797a78803cf994932ba68fe8d624bc85e896
Exia <nohitokaze@gmail.com>
2024-04-17 06:56:02 -0400

    Wiring up context

--- bookstore/src/components/BookShelf.js
+++ bookstore/src/components/BookShelf.js
@@ -1,7 +1,11 @@
 import Book from "./Book";
+import { useContext } from "react";
+import LibraryContext from "../context/books";
 function BookShelf({books, onDelete, onEdit}) {
+    const message = useContext(LibraryContext);
     return (
         <div className="book-list">
+            <h5>{message}</h5>
             <ol>
                 {books.map(bk => {
                     return (<li key={bk.id}>
--- bookstore/src/index.js
+++ bookstore/src/index.js
@@ -2,10 +2,13 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import LibraryContext from './context/books';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<LibraryContext.Provider value={"Hello World"}>
+			<App />
+		</LibraryContext.Provider>
 	</React.StrictMode>
 );
